6c6df264d05fd026cd30eb801a17943795077271
Josua Frank <frank.josua@gmail.com>
2020-10-20 09:28:10 +0200

    Update vue.js

--- src/js/vue.js
+++ src/js/vue.js
@@ -4,14 +4,14 @@
 
 // Vue
 import { createApp } from 'vue'
-// Vue Router
-import router from '@/js/vue-router'
 //Pixina
 import Pixina from '@/Pixina.vue'
-//Vuetify
-import vuetify from '@/plugins/vuetify'
+// Vue Router
+import router from '@/js/vue-router'
 //Vuex
 import store from '@/js/vuex'
+//Vuetify
+import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 
